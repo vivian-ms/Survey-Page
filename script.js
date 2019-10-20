10c8091a302080e86d1f4aa082e8d2b3d92a8745
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2019-10-19 23:37:59 -0700

    Added rating circles

--- script.js
+++ script.js
@@ -5,4 +5,19 @@ $(function() {
     evt.preventDefault();
     alert('Thank you for your feedback!');
   });
+
+  // Event listener for clicking rating circle
+  $('.rating-container').on('click', '.rating-circle' , function(evt) {
+    // 1) Remove .rating-selected from all rating circles
+    $('.rating-circle').removeClass('rating-selected');
+
+    // 2) Add .rating-selected to the clicked rating circle and all rating circles before it
+    $(this).addClass('rating-selected');
+    $(this).prevAll('.rating-circle').addClass('rating-selected');
+  });
+
+  // Event listener for reset button
+  $('#reset').on('click', function(evt) {
+    $('.rating-circle').removeClass('rating-selected');
+  });
 });
